2e25b20f7d6e28b735ef25ec2cf018fa1ef79d63
Bruce Luo <30612681+Feezy15@users.noreply.github.com>
2019-06-11 17:49:18 -0500

    test header

---
+++ js/particles.js
@@ -0,0 +1,7 @@
+window.onLoad = function() {
+    Particles.init({
+        selector: '.background',
+        sizeVariations: 5,
+        color: ["#baa7b0", "#b2abbf", "#b1b5c8", "bfd5e2", "c7ebf0"]
+    });
+};
