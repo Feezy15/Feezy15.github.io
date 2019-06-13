2fa9611d2896df36d64dd05bbe8bc14a7ae89300
Bruce Luo <30612681+Feezy15@users.noreply.github.com>
2019-06-12 20:58:21 -0500

    cleaned up header, about

--- js/particles.js
+++ js/particles.js
@@ -1,7 +1,7 @@
-window.onLoad = function() {
+var particles = 
     Particles.init({
         selector: '.background',
-        sizeVariations: 5,
+        sizeVariations: 7,
+        maxParticles: 100,
         color: ["#baa7b0", "#b2abbf", "#b1b5c8", "bfd5e2", "c7ebf0"]
     });
-};
