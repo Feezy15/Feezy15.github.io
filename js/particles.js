0e3f8b045ce40eb7c0887b05ece887a520103ba4
Bruce Luo <30612681+Feezy15@users.noreply.github.com>
2019-06-15 00:20:28 -0500

    add navbar

--- js/particles.js
+++ js/particles.js
@@ -3,5 +3,25 @@ var particles =
         selector: '.background',
         sizeVariations: 7,
         maxParticles: 100,
-        color: ["#baa7b0", "#b2abbf", "#b1b5c8", "bfd5e2", "c7ebf0"]
+        color: ["#baa7b0", "#b2abbf", "#b1b5c8", "bfd5e2", "c7ebf0"],
+        responsive: [
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 50
+                }
+            }
+            , {
+                breakpoint: 525,
+                options: {
+                    maxParticles: 25
+                }
+            }
+            , {
+                breakpoint: 320,
+                options: {
+                    maxParticles: 12
+                }
+            }
+        ]
     });
